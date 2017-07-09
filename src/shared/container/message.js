bde177e477be5d31235986f460254c89b7174f4b
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-07-08 19:04:58 -0500

    makes little changes

--- src/shared/container/message.js
+++ src/shared/container/message.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import Message from '../component/message'
 
 const mapStateToProps = state => ({
-  message: state.message,
+  message: state.hello.message,
 })
 
 export default connect(mapStateToProps)(Message)
