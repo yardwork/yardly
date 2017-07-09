9c3e453a3bd91ed72e132005f1cf0f839e62421c
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-07-08 19:04:58 -0500

    makes little changes

--- src/shared/component/message.jsx
+++ src/shared/component/message.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Message = Props =>
-  <p>{Props.message}</p>
+const Message = props =>
+  (<div>
+    <p>{props.message}</p>
+  </div>)
+
+Message.propTypes = {
+  message: PropTypes.string.isRequired,
+}
 
 export default Message
