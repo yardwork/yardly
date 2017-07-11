a4a89e6dffd640ba42b14697883c806ec16e4084
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-07-11 12:10:26 -0500

    adds functionality to server

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,22 @@
 import compression from 'compression'
 import express from 'express'
+import bodyParser from 'body-parser'
 
+import usersRoute from './routes/users'
+import workersRoute from './routes/workers'
 import routing from './routing'
 import { WEB_PORT, STATIC_PATH } from '../shared/config'
 import { isProd } from '../shared/util'
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(compression())
+app.use(bodyParser.json())
 app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
 
+app.use('/api', workersRoute, usersRoute)
 routing(app)
 
 app.listen(WEB_PORT, () => {
