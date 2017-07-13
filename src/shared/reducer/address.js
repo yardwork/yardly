bb8070bf55c17a5050c44bc9f70578b705f7b2d9
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-07-13 18:34:53 -0500

    adds features

--- src/shared/reducer/address.js
+++ src/shared/reducer/address.js
@@ -1,21 +1,39 @@
+
+import { ADD_ADDRESS_ASYNC_REQUEST, ADD_ADDRESS_ASYNC_SUCCESS, ADD_ADDRESS_ASYNC_FAILURE } from '../action/address'
+
 const initialState = {
-  address: {
+  user: {
+    _id: "59656ea6fdf5df76da38772c",
+    username: "stueeer",
+    password: "passwordddss",
+    __v: 0,
+    addresses: [{
+      address: '',
+      city: '',
+      state: '',
+      zipCode: '',
+    }],
+  },
+  addresses: [{
     address: '',
     city: '',
     state: '',
     zipCode: '',
-  },
+  }],
+  loading: false,
 }
 
 const addressReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'LOAD':
-      return { ...state, data: action.data }
+    case ADD_ADDRESS_ASYNC_REQUEST:
+      return { ...state, loading: true }
+    case ADD_ADDRESS_ASYNC_SUCCESS:
+      return { ...state, user: action.payload }
+    case ADD_ADDRESS_ASYNC_FAILURE:
+      return { ...state, loading: false }
     default:
       return state
   }
 }
-
-// export const load = data => ({ type: 'LOAD', data })
 
 export default addressReducer
